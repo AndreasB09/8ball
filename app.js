f4db28af0eda8d6d38777eb4bbd420e51983170f
AndreasB09 <andreasb09.kodehode@gmail.com>
2024-01-09 15:41:36 +0100

    final

--- app.js
+++ app.js
@@ -14,11 +14,11 @@ const answers = [
   "Concentrate and ask again",
   "Very doubtful",
   "You may rely on it",
-  "As I see it, yes"
+  "As I see it, yes",
 ];
 
-const resultElement = document.getElementById('result')
-const shakeBtn = document.querySelector('.shake-btn')
+const resultElement = document.getElementById("result");
+const shakeBtn = document.querySelector(".shake-btn");
 
 shakeBtn.addEventListener("click", function () {
   const randomIndex = Math.floor(Math.random() * answers.length);
@@ -26,20 +26,20 @@ shakeBtn.addEventListener("click", function () {
 });
 
 function changeImage(id) {
-  document.querySelectorAll('.image-list li img').forEach(function (img) {
-      img.src = img.src.replace('_yellow', '_white');
+  document.querySelectorAll(".image-list li img").forEach(function (img) {
+    img.src = img.src.replace("_yellow", "_white");
   });
 
-  let imgElement = document.getElementById(id).querySelector('img');
+  let imgElement = document.getElementById(id).querySelector("img");
   imgElement.src = `./images/${id}_${getColor(id)}.svg`;
 
-  document.querySelectorAll('.image-list li').forEach(function (item) {
-      item.classList.remove('active');
+  document.querySelectorAll(".image-list li").forEach(function (item) {
+    item.classList.remove("active");
   });
 
-  document.getElementById(id).classList.add('active');
+  document.getElementById(id).classList.add("active");
 
-  let styleElement = document.createElement('style');
+  let styleElement = document.createElement("style");
   document.head.appendChild(styleElement);
 
   styleElement.textContent = `
@@ -56,44 +56,44 @@ function changeImage(id) {
     }
   `;
 
-  document.querySelector('.circle').classList.add('active');
+  document.querySelector(".circle").classList.add("active");
 }
 
 function getPulseStartColor(id) {
   switch (id) {
-      case 'water':
-          return '#00B4D8';
-      case 'leaf':
-          return '#9EF01A';
-      case 'air':
-          return '#C77DFF';
-      default:
-          return '#FFB703';
+    case "water":
+      return "#00B4D8";
+    case "leaf":
+      return "#9EF01A";
+    case "air":
+      return "#C77DFF";
+    default:
+      return "#FFB703";
   }
 }
 
 function getPulseEndColor(id) {
   switch (id) {
-      case 'water':
-          return '#0077B6';
-      case 'leaf':
-          return '#70E000';
-      case 'air':
-          return '#9D4EDD';
-      default:
-          return '#FB8500';
+    case "water":
+      return "#0077B6";
+    case "leaf":
+      return "#70E000";
+    case "air":
+      return "#9D4EDD";
+    default:
+      return "#FB8500";
   }
 }
 
 function getColor(id) {
   switch (id) {
-      case 'water':
-          return 'blue';
-      case 'leaf':
-          return 'green';
-      case 'air':
-          return 'purple';
-      default:
-          return 'yellow';
+    case "water":
+      return "blue";
+    case "leaf":
+      return "green";
+    case "air":
+      return "purple";
+    default:
+      return "yellow";
   }
 }
